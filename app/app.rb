63de3de3e507fce12e2a75f22555cd4992421597
liskowsky <karollebiedzinski@gmail.com>
2016-07-27 09:28:33 +0100

    Ch11 Creating new link feature added

--- app/app.rb
+++ app/app.rb
@@ -2,8 +2,18 @@ require_relative 'models/link'
 require 'sinatra/base'
 
 class BookmarkManager < Sinatra::Base
+
   get '/links' do
     @links = Link.all
     erb :'links/index'
+  end
+
+  get '/links/new' do
+    erb :'links/new'
+  end
+
+  post '/links' do
+    Link.create(url: params[:url], title: params[:title])
+    redirect '/links'
   end
 end
